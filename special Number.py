7e30ceb00fe81d5817ccba547b1eaf63ac39355f
Rishi329 <117821279+Rishi329@users.noreply.github.com>
2023-06-13 13:12:27 +0530

    if elif else demo

---
+++ special Number.py
@@ -0,0 +1,11 @@
+num = int(input("Enter number :"))
+if (num < 0):
+  print("the number is nigative")
+elif (num == 0):
+  print("The number is zero")
+elif (num == 999):
+  print("the number is special")  
+else:
+  print("the number is positive")
+
+print("I am happy now")  
