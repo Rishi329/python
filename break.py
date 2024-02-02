5f4df383d3acdb79cc0ec857163237db174886a2
rishi329 <rishigarad329@gmail.com>
2024-02-02 15:29:42 +0530

    new programs

---
+++ break.py
@@ -0,0 +1,6 @@
+#practical n0 - 12-B
+#use of break statement
+for i in range(1,11):
+    if (i==6):
+        break
+    print(i)
---
+++ practical no-12.py
@@ -0,0 +1,6 @@
+#practical n0 - 12-B
+#use of continue statement
+for i in range(1,11):
+    if (i==6):
+        continue
+    print(i)
